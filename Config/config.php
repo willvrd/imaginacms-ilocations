36c0721138fd58d742bc21368272388e4cb942e1
Marcos Rativa <marcosr@imaginacolombia.com>
2019-01-08 16:57:31 -0500

    Preparing version 0.2.8

--- Config/config.php
+++ Config/config.php
@@ -1,5 +1,13 @@
 <?php
 
 return [
-    'name' => 'Ilocations'
+    'name' => 'Ilocations',
+
+    'relations' => [
+ //     'entity'=>[
+//        'extension' => function ($self) {
+//            return $self->belongsTo(EntityExtension::class, 'foreign_id', 'id')->first();
+//        }
+//    ]
+    ],
 ];
--- Entities/Country.php
+++ Entities/Country.php
@@ -42,4 +42,21 @@ class Country extends Model
   {
     return $this->hasMany(GeozonesCountries::class);
   }
+
+    public function __call($method, $parameters)
+    {
+        #i: Convert array to dot notation
+        $config = implode('.', ['asgard.ilocations.config.relations.country', $method]);
+
+        #i: Relation method resolver
+        if (config()->has($config)) {
+            $function = config()->get($config);
+            $bound = $function->bindTo($this);
+
+            return $bound();
+        }
+
+        #i: No relation found, return the call to parent (Eloquent) to handle it.
+        return parent::__call($method, $parameters);
+    }
 }
