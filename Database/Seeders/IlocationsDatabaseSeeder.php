a4363d68a1074a02df897ac67b67124a2e226289
JCEC007 <jcec007@gmail.com>
2020-10-08 16:35:35 -0500

    +update: add truncate of tralatables

--- Database/Seeders/IlocationsDatabaseSeeder.php
+++ Database/Seeders/IlocationsDatabaseSeeder.php
@@ -15,14 +15,17 @@ class IlocationsDatabaseSeeder extends Seeder
    */
   public function run()
   {
-  
-    DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-    DB::table('ilocations__countries')->truncate();
     
-    DB::table('ilocations__provinces')->truncate();
+    \DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+    \DB::table('ilocations__countries')->truncate();
+    \DB::table('ilocations__country_translations')->truncate();
     
-    DB::table('ilocations__cities')->truncate();
-    DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+    \DB::table('ilocations__provinces')->truncate();
+    \DB::table('ilocations__province_translations')->truncate();
+    
+    \DB::table('ilocations__cities')->truncate();
+    \DB::table('ilocations__city_translations')->truncate();
+    \DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     
     $this->call(CountryTableSeeder::class);
     $this->call(ProvinceTableSeeder::class);
