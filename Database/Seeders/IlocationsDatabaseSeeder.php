d41e4264c5d792eda13154a52eb6476915935d0a
William Verde <williamverde10@gmail.com>
2023-03-23 17:23:06 -0400

    Deleted: old comment in location seeder

--- Database/Seeders/IlocationsDatabaseSeeder.php
+++ Database/Seeders/IlocationsDatabaseSeeder.php
@@ -16,25 +16,11 @@ class IlocationsDatabaseSeeder extends Seeder
   public function run()
   {
 
-//    \DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-//    \DB::table('ilocations__countries')->truncate();
-//    \DB::table('ilocations__country_translations')->truncate();
-//
-//    \DB::table('ilocations__provinces')->truncate();
-//    \DB::table('ilocations__province_translations')->truncate();
-//
-//    \DB::table('ilocations__cities')->truncate();
-//    \DB::table('ilocations__city_translations')->truncate();
-//
-//    //\DB::table('ilocations__geozones')->truncate();
-//    //\DB::table('ilocations__geozones_countries_provinces')->truncate();
-//
-//    \DB::statement('SET FOREIGN_KEY_CHECKS=1;');
-
     $this->call(IlocationsModuleTableSeeder::class);
     $this->call(CountryTableSeeder::class);
     $this->call(ProvinceTableSeeder::class);
     $this->call(CityTableSeeder::class);
     $this->call(GeozoneTableSeeder::class);
+    
   }
 }
